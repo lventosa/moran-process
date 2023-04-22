28b91eecb54e847a126a677c96bde425aa9dff62
lventosa <laura.ventosa01@estudiant.upf.edu>
2023-04-23 00:36:00 +0200

    Final commit

--- moran_simulation.py
+++ moran_simulation.py
@@ -10,7 +10,6 @@ from moran_process import MoranProcess
 
 INITIAL_STATE = 5
 POPULATION_SIZE = 10
-SEED = 123
 
 
 def get_other_group_population_counts(
@@ -35,10 +34,39 @@ def get_other_group_population_counts(
     return b_array
 
 
+def fixation_probability(num_repetitions: int, initial_state: int) -> float:
+    """
+    Computes the probability of fixation of a mutation in a population.
+
+    :param num_repetitions: number of repetitions of the simulation
+    :type num_repetitions: int
+
+    :param initial_state: initial population count of group A
+    :type initial_state: int
+
+    :return: probability of fixation
+    :rtype: float
+    """
+    fixation_count = 0
+
+    for rep in range(num_repetitions):
+        moran_process = MoranProcess(
+            population_size=POPULATION_SIZE, 
+            seed=rep, 
+            initial_state=initial_state,
+        )
+        neutral_drift_array_a = moran_process.simulate_moran_process()
+
+        if neutral_drift_array_a[-1] == POPULATION_SIZE:
+            fixation_count += 1
+
+    return fixation_count/num_repetitions
+
+
 if __name__ == '__main__':
     moran_process = MoranProcess(
         population_size=POPULATION_SIZE, 
-        seed=SEED, 
+        seed=123, 
         initial_state=INITIAL_STATE,
     )
     neutral_drift_array_a = moran_process.simulate_moran_process()
@@ -49,4 +77,17 @@ if __name__ == '__main__':
 
     plt.plot(np.column_stack((neutral_drift_array_a, neutral_drift_array_b)))
     plt.legend(loc='upper left', labels=('A', 'B'))
+    plt.show()
+
+    probabilities = [
+        fixation_probability(num_repetitions=500, initial_state=i) for i in range(1, POPULATION_SIZE)
+    ]
+    plt.scatter(range(1, POPULATION_SIZE), probabilities, label='Experimental probability')
+    plt.plot(
+        range(1, POPULATION_SIZE), [i/POPULATION_SIZE for i in range(1, POPULATION_SIZE)], 
+        label='Theoretical probability', linestyle='dashed'
+    )
+    plt.xlabel("$i$")
+    plt.ylabel("$i/N$")
+    plt.legend(loc='upper left')
     plt.show()
